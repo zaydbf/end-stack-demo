67ea481bbde02259a6453eb901a2c1aaf00cbdf4
zaydbf <zaydbenfathel@gmail.com>
2025-11-14 17:19:20 +0100

    Added : Scripts

---
+++ assembler.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+fileName="${1%%.*}" # remove .s extension
+
+nasm -f elf64 ${fileName}".s"
+ld ${fileName}".o" -o ${fileName}
---
+++ shellcode.py
@@ -0,0 +1,8 @@
+import sys
+from pwn import *
+
+context(os="linux", arch="amd64", log_level="error")
+
+file = ELF(sys.argv[1])
+shellcode = file.section(".text")
+print(shellcode.hex())
